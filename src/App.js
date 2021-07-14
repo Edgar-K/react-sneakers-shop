9e79be0114956d03ab70a380419a21a23b07108c
Edgar Kocharyan <58892166+Edgar-K@users.noreply.github.com>
2021-07-14 23:52:24 +0200

    installing npm package with CSS classes macro-css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
-
 function App() {
   return (
-    <div > New Project</div>
-  );
+    <div className="wrapper" >
+      <header>
+        <div className="headerLeft">
+          <img width={40}  height={40} src="/img/logo.png"  alt="logo"/>
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>The best Shop for Sneakers</p>
+          </div>
+        </div>
+        <ul className="headerInfo">
+          <li>
+          <img width={18}  height={18} src="/img/user.svg" alt="user-icon" />
+            <span>56 EUR</span>
+          </li>
+          <li>
+          <img width={18}  height={18} src="/img/cart.svg"  alt="cart-icon"/>
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>All Sneakers</h1>
+        .....
+      </div>
+    </div>
+  )
 }
 
 export default App;
