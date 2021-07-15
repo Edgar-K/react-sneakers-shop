9bbc8922cca83d1e4e1635d980926d35d624b6d1
Edgar Kocharyan <58892166+Edgar-K@users.noreply.github.com>
2021-07-15 23:24:23 +0200

    added Inter fonts from fonts.google,using link into index.html

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 function App() {
   return (
-    <div className="wrapper" >
-      <header className="d-flex justify-between">
+    <div className="wrapper clear" >
+      <header className="d-flex justify-between p-40 align-center">
         <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" alt="logo" />
           <div className="headerInfo">
-            <h3 >React Sneakers</h3>
+            <h3 className="text-uppercase">React Sneakers</h3>
             <p>The best Shop for Sneakers</p>
           </div>
         </div>
-        <ul className="headerInfo">
-          <li>
+        <ul className="d-flex">
+          <li className="mr-30">
             <img width={18} height={18} src="/img/user.svg" alt="user-icon" />
             <span>56 EUR</span>
           </li>
@@ -19,7 +19,7 @@ function App() {
           </li>
         </ul>
       </header>
-      <div className="content">
+      <div className="content p-40">
         <h1>All Sneakers</h1>
         .....
       </div>
