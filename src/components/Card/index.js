598831ea1d3922bd77e57e1af004ae80dfc64ffd
Edgar Kocharyan <58892166+Edgar-K@users.noreply.github.com>
2021-07-21 23:37:11 +0200

    handling onClick event on Cart plus button

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import styles from './Card.module.scss'
 
 function Cart(props) {
+    const [isAdded, setIsAdded] = React.useState();
 
+    const onClickPlus = () => {
+        setIsAdded(true);
+    }
 
     return (
         <div className={styles.card}>
             <div className={styles.favorite} onClick={props.onFavorite} >
-                <img src="/img/heart-unliked.svg" alt="unliked" width={15} height={15}   />
+                <img src="/img/heart-unliked.svg" alt="unliked" width={15} height={15} />
             </div>
 
             <img src={props.imageUrl} alt="Sneakers" width={170} height={112} />
@@ -16,9 +21,9 @@ function Cart(props) {
                     <span>Price</span>
                     <b>€ {props.price}</b>
                 </div>
-                <button className="button" onClick={props.onPlus} >
-                    <img src="/img/btn-plus.svg" alt="plus" width={20} height={20} />
-                </button>
+
+                <img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="plus" width={20} height={20} />
+
             </div>
         </div>
     );
