685c74770f71a39f9f04a3968d8ea0eced8b0ff9
Edgar Kocharyan <58892166+Edgar-K@users.noreply.github.com>
2021-07-23 01:26:02 +0200

    implementing and using mock APIs , MockAPI is a simple tool that lets you easily mock up APIs

--- src/App.js
+++ src/App.js
@@ -4,30 +4,11 @@ import Header from './components/Header'
 import Drawer from './components/Drawer'
 
 
-const arr = [
-  {
-    title: 'Nike Sportswear',
-    price: 59.99,
-    imageUrl: '/img/sneakers/1.jpg'
-  },
-  {
-    title: 'FLEX EXPERIENCE RUN 10',
-    price: 55.99,
-    imageUrl: '/img/sneakers/2.jpg'
-  },
-  {
-    title: 'Air Max Bella Tr 3',
-    price: 89.99,
-    imageUrl: '/img/sneakers/3.jpg'
-  },
-  {
-    title: 'KD TREY 5 IX',
-    price: 99,
-    imageUrl: '/img/sneakers/4.jpg'
-  }
-]
+
 
 function App() {
+
+  const [items, setItems] = React.useState([]);
 
   const [cartOpened, setCartOpened] = React.useState(false);
 
@@ -43,8 +24,8 @@ function App() {
             <input placeholder="Search..." />
           </div>
         </div>
-        <div className="d-flex justify-between">
-          {arr.map((obj) => (
+        <div className="d-flex flex-wrap">
+          {items.map((obj) => (
             <Cart
               title={obj.title}
               price={obj.price}
